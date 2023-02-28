41977d9bef3f27228669a3e946b7951b3d6f44a1
Andreas Tsarida <tsaridas@users.noreply.github.com>
2023-02-28 21:42:19 +0100

    update listener

--- eventlistener/event-listener.go
+++ eventlistener/event-listener.go
@@ -9,15 +9,24 @@ import (
 	"strings"
 )
 
+func b2s(bs []uint8) string {
+        ba := []byte{}
+        for _, b := range bs {
+                ba = append(ba, byte(b))
+        }
+        return string(ba)
+}
+
 func reader(r io.Reader) {
 	dec := msgpack.NewDecoder(r)
 	for {
 		var m1 map[string]interface{}
 		dec.Decode(&m1)
-		m1_1 := m1["body"].(string)
+		m1_1 := m1["body"].([]uint8)
+		s_var := B2S(m1_1)
 		var m2 map[string]interface{}
 		resultAll := fmt.Sprint(m1_1)
-		resultList := strings.SplitN(resultAll, "\n\n", 2)
+		resultList := strings.Split(s_var, "\n\n")
 		tag := resultList[0]
 		byteResult := []byte(resultList[1])
 		_ = msgpack.Unmarshal(byteResult, &m2)
